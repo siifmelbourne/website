cc38fae308284c4801eff9444f796a47a92dc0a6
flatplum <flatplum@proton.me>
2026-03-21 22:33:05 +1100

    feat: add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,14 @@ hooks: {
       title: 'Social Impact Investment Fund', // default fallback title
       htmlAttrs: {
         lang: 'en'
-      }
+      },
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-48x48.png', sizes: '48x48' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-192x192.png', sizes: '192x192' },
+        { rel: 'apple-touch-icon', href: '/favicon-180x180.png' },
+      ],
     }
   },
 
