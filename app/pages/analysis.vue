0ec15ddf6bac82bbe6160331d0401d40dddd51db
aledrinyth <58565713+aledrinyth@users.noreply.github.com>
2026-06-01 16:00:49 +0800

    Added temporary analysis page
    Added an iframe that points to the container app with the streamlit dashboards
    
    Signed-off-by: aledrinyth <58565713+aledrinyth@users.noreply.github.com>

---
+++ app/pages/analysis.vue
@@ -0,0 +1,24 @@
+<template>
+  <Banner title="streamlit dashboards" />
+  <main class="main">
+    <iframe
+      class="embed"
+      src="https://siif-dashboard-11-05-26.happydesert-94097712.japaneast.azurecontainerapps.io"
+      title="Dashboard"
+      loading="lazy"
+      allowfullscreen
+    />
+  </main>
+</template>
+
+<style scoped>
+.main {
+  padding: 0;
+}
+.embed {
+  display: block;
+  width: 100%;
+  height: calc(100vh - 4rem); /* tweak to taste */
+  border: 0;
+}
+</style>
